a315c21a4c00fbc862b510f3e5daae0f76ee8533
MARKO JOVICIC <markoj2203@gmail.com>
2021-01-03 20:54:56 +0100

    get movie list with axios and display it to BlogPost component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div className="jumbotron">
-      <h1>List of blog posts</h1>
+      <h1>Movie List</h1>
     </div>
   );
 }
